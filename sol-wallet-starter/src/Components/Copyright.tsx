016151993cc1f2444e5bfe9d6b384d09a1b3e372
Jack-R-Long <jack.richard.long@gmail.com>
2022-03-10 16:20:41 -0600

    First commit

---
+++ sol-wallet-starter/src/Components/Copyright.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Typography, Link } from "@mui/material";
+
+
+export default function Copyright() {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Company
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
